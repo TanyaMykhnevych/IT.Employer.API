7b06397141623cd5b6c92fffca468867f885a441
tatyana.mykhnevych <tatyana.mykhnevych@gmail.com>
2020-04-13 20:35:53 +0300

    Added PaginationModelDTO.cs

---
+++ IT.Employer.Entities/Models/Base/PaginationModelDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IT.Employer.Entities.Models.Base
+{
+    public class PaginationModelDTO
+    {
+        [Required]
+        [Range(0, Int32.MaxValue)]
+        public Int32 Page { get; set; }
+
+        [Required]
+        [Range(0, Int32.MaxValue)]
+        public Int32 PerPage { get; set; }
+    }
+}
